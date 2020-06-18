ce10caf5033131bfc88878892b0a235384d764cd
Jan bjørndalen <jan.bjorndalen@gmail.com>
2020-06-18 10:29:29 +0200

    New handler

---
+++ get-customer-by-org-number/src/main/java/no/unit/nva/customer/get/GetCustomerByOrgNumberHandler.java
@@ -0,0 +1,66 @@
+package no.unit.nva.customer.get;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.lambda.runtime.Context;
+import no.unit.nva.customer.ObjectMapperConfig;
+import no.unit.nva.customer.exception.InputException;
+import no.unit.nva.customer.model.Customer;
+import no.unit.nva.customer.service.CustomerService;
+import no.unit.nva.customer.service.impl.DynamoDBCustomerService;
+import nva.commons.exceptions.ApiGatewayException;
+import nva.commons.handlers.ApiGatewayHandler;
+import nva.commons.handlers.RequestInfo;
+import nva.commons.utils.Environment;
+import nva.commons.utils.JacocoGenerated;
+import nva.commons.utils.RequestUtils;
+import org.apache.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GetCustomerByOrgNumberHandler extends ApiGatewayHandler<Void, Customer> {
+
+    public static final String ORG_NUMBER = "orgNumber";
+
+    private final CustomerService customerService;
+    private static final Logger logger = LoggerFactory.getLogger(GetCustomerByOrgNumberHandler.class);
+
+    /**
+     * Default Constructor for GetCustomerHandler.
+     */
+    @JacocoGenerated
+    public GetCustomerByOrgNumberHandler() {
+        this(new DynamoDBCustomerService(
+            AmazonDynamoDBClientBuilder.defaultClient(),
+            ObjectMapperConfig.objectMapper,
+            new Environment()
+        ), new Environment());
+    }
+
+    /**
+     * Constructor for CreateCustomerHandler.
+     *
+     * @param customerService customerService
+     * @param environment   environment
+     */
+    public GetCustomerByOrgNumberHandler(CustomerService customerService, Environment environment) {
+        super(Void.class, environment, logger);
+        this.customerService = customerService;
+    }
+
+    @Override
+    protected Customer processInput(Void input, RequestInfo requestInfo, Context context)
+        throws ApiGatewayException {
+        String orgNumber;
+        try {
+            orgNumber = RequestUtils.getPathParameter(requestInfo, ORG_NUMBER);
+        } catch (IllegalArgumentException e) {
+            throw new InputException(e.getMessage(), e);
+        }
+        return customerService.getCustomerByOrgNumber(orgNumber);
+    }
+    
+    @Override
+    protected Integer getSuccessStatusCode(Void input, Customer output) {
+        return HttpStatus.SC_OK;
+    }
+}
---
+++ get-customer-by-org-number/src/test/java/no/unit/nva/customer/get/GetCustomerByOrgNumberHandlerTest.java
@@ -0,0 +1,116 @@
+package no.unit.nva.customer.get;
+
+import static no.unit.nva.customer.testing.TestHeaders.getErrorResponseHeaders;
+import static no.unit.nva.customer.testing.TestHeaders.getRequestHeaders;
+import static no.unit.nva.customer.testing.TestHeaders.getResponseHeaders;
+import static nva.commons.handlers.ApiGatewayHandler.ALLOWED_ORIGIN_ENV;
+import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.zalando.problem.Status.BAD_REQUEST;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.UUID;
+import no.unit.nva.customer.ObjectMapperConfig;
+import no.unit.nva.customer.model.Customer;
+import no.unit.nva.customer.service.CustomerService;
+import no.unit.nva.testutils.HandlerRequestBuilder;
+import nva.commons.handlers.GatewayResponse;
+import nva.commons.utils.Environment;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.zalando.problem.Problem;
+
+public class GetCustomerByOrgNumberHandlerTest {
+
+    public static final String WILDCARD = "*";
+    public static final String REQUEST_ID = "requestId";
+    public static final String SAMPLE_ORG_NUMBER = "123";
+    public static final String EXPECTED_ERROR_MESSAGE = "Missing from pathParameters: orgNumber";
+
+    private ObjectMapper objectMapper = ObjectMapperConfig.objectMapper;
+    private CustomerService customerServiceMock;
+    private Environment environmentMock;
+    private GetCustomerByOrgNumberHandler handler;
+    private ByteArrayOutputStream outputStream;
+    private Context context;
+
+    /**
+     * Setting up test environment.
+     */
+    @BeforeEach
+    @SuppressWarnings("unchecked")
+    public void setUp() {
+        customerServiceMock = mock(CustomerService.class);
+        environmentMock = mock(Environment.class);
+        when(environmentMock.readEnv(ALLOWED_ORIGIN_ENV)).thenReturn(WILDCARD);
+        handler = new GetCustomerByOrgNumberHandler(customerServiceMock, environmentMock);
+        outputStream = new ByteArrayOutputStream();
+        context = Mockito.mock(Context.class);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void requestToHandlerReturnsCustomer() throws Exception {
+        Customer customer = new Customer.Builder()
+            .withIdentifier(UUID.randomUUID())
+            .withFeideOrganizationId(SAMPLE_ORG_NUMBER)
+            .build();
+        when(customerServiceMock.getCustomerByOrgNumber(SAMPLE_ORG_NUMBER)).thenReturn(customer);
+
+        Map<String, String> pathParameters = Map.of(GetCustomerByOrgNumberHandler.ORG_NUMBER, SAMPLE_ORG_NUMBER);
+        InputStream inputStream = new HandlerRequestBuilder<Customer>(objectMapper)
+            .withBody(customer)
+            .withHeaders(getRequestHeaders())
+            .withPathParameters(pathParameters)
+            .build();
+        handler.handleRequest(inputStream, outputStream, context);
+
+        GatewayResponse<Customer> actual = objectMapper.readValue(
+            outputStream.toByteArray(),
+            GatewayResponse.class);
+
+        GatewayResponse<Customer> expected = new GatewayResponse<>(
+            objectMapper.writeValueAsString(customer),
+            getResponseHeaders(),
+            HttpStatus.SC_OK
+        );
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void requestToHandlerWithEmptyOrgNumberReturnsBadRequest() throws Exception {
+
+        InputStream inputStream = new HandlerRequestBuilder<Customer>(objectMapper)
+            .withHeaders(getRequestHeaders())
+            .build();
+
+        handler.handleRequest(inputStream, outputStream, context);
+
+        GatewayResponse<Problem> actual = objectMapper.readValue(
+            outputStream.toByteArray(),
+            GatewayResponse.class);
+
+        GatewayResponse<Problem> expected = new GatewayResponse<>(
+            Problem.builder()
+                .withStatus(BAD_REQUEST)
+                .withTitle(BAD_REQUEST.getReasonPhrase())
+                .withDetail(EXPECTED_ERROR_MESSAGE)
+                .with(REQUEST_ID, null)
+                .build(),
+            getErrorResponseHeaders(),
+            SC_BAD_REQUEST
+        );
+
+        assertEquals(expected, actual);
+    }
+}
